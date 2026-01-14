502592d367ccc10900962bdff24ac307250fe83f
EGM <elena.guthauser@mng.ch>
2026-01-14 17:48:41 +0000

    New db with photos

--- db/recipe_db.sql
+++ db/recipe_db.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(250) NOT NULL UNIQUE,
@@ -111,43 +110,48 @@ INSERT INTO ingredient (id, ingredient_name, vegetarisch, vegan, laktose, ingred
 
 
 
-INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben) VALUES (
+INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben, recipe_photo) VALUES (
     1,
     'Hörnli-Auflauf',
     '1. Ofen auf 200°C vorheizen.\n2. Hörnli nach Packungsanleitung al dente kochen.\n3. Währenddessen Schinken in kleine Würfel schneiden.\n4. Käse fein reiben.\n5. Eier mit Milch, Salz, Pfeffer und Muskatnuss verquirlen.\n6. Abgetropfte Hörnli mit Schinken und der Hälfte des Käses vermischen.\n7. In eine gefettete Auflaufform geben.\n8. Eiermilch darüber giessen.\n9. Mit restlichem Käse bestreuen.\n10. Im vorgeheizten Ofen 25-30 Minuten backen bis goldbraun und stockt.',
-    '4 Portionen'
+    '4 Portionen',
+  'https://i.pinimg.com/564x/a9/69/30/a96930127a432f97e2cf060a07758114.jpg'
 );
 
 
-INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben) VALUES (
+INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben, recipe_photo) VALUES (
     2,
     'Bananen Pancakes',
     '1. Bananen mit einer Gabel zu einem Mus zerdrücken.\n2. Eier hinzufügen und gut verrühren.\n3. Mehl, Backpulver und Zimt unterrühren.\n4. Milch nach und nach hinzufügen, bis ein glatter, dickflüssiger Teig entsteht.\n5. Etwas Öl in einer beschichteten Pfanne erhitzen.\n6. Pro Pancake 1-2 Esslöffel Teig in die Pfanne geben.\n7. Bei mittlerer Hitze backen, bis sich Blasen auf der Oberfläche bilden.\n8. Wenden und von der anderen Seite goldbraun backen.\n9. Warm mit Ahornsirup servieren.',
-    '2 Portionen'
+    '2 Portionen',
+  'https://www.gaumenfreundin.de/wp-content/uploads/2023/01/Bananen-Pancakes.jpg'
 );
 
 
-INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben) VALUES (
+INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben, recipe_photo) VALUES (
     3,
     'Rösti',
     '1. Kartoffeln schälen und grob reiben.\n2. Die geriebenen Kartoffeln in einem Küchentuch kräftig ausdrücken, um möglichst viel Feuchtigkeit zu entfernen.\n3. Zwiebel fein hacken und mit den Kartoffeln vermischen.\n4. Salz und Pfeffer hinzufügen.\n5. Butter oder Öl in einer Pfanne erhitzen.\n6. Kartoffelmasse in die heisse Pfanne geben und gleichmässig flach drücken.\n7. Bei mittlerer Hitze 10-12 Minuten braten, bis die Unterseite goldbraun ist.\n8. Rösti auf einen Teller stürzen, zurück in die Pfanne gleiten lassen und weitere 10-12 Minuten braten.\n9. Direkt servieren.',
-    '3 Portionen'
+    '3 Portionen',
+  'https://rezepte.lemenu.ch/images/promo_images/spree/tx_netvlmerdb_recipes/000/009/576/promo/LM202003_08.jpg?1582701175'
 );
 
 
-INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben) VALUES (
+INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben, recipe_photo) VALUES (
     4,
     'Rüebli Suppe',
     '1. Zwiebel und Knoblauch fein hacken.\n2. Karotten schälen und in Scheiben schneiden.\n3. In einem grossen Topf Butter schmelzen.\n4. Zwiebel und Knoblauch glasig dünsten.\n5. Karotten hinzufügen und 5 Minuten mitdünsten.\n6. Mit Gemüsebrühe ablöschen.\n7. 20-25 Minuten köcheln lassen, bis die Karotten weich sind.\n8. Vom Herd nehmen und mit einem Stabmixer fein pürieren.\n9. Sahne einrühren und mit Salz, Pfeffer und frischem Ingwer abschmecken.\n10. Nochmals kurz erwärmen, nicht mehr kochen lassen.',
-    '4 Portionen'
+    '4 Portionen',
+  'https://recipeimages.migros.ch/crop/v-w-1200-h-630-a-center_center/4b853964447ca50dfcea0f7226f5b9484451dcb2/rueeblisuppe-0-16-9.jpg'
 );
 
 
-INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben) VALUES (
+INSERT INTO recipes (recipe_id, recipe_name, recipe_instruction, recipe_mengenangaben, recipe_photo) VALUES (
     5,
     'Milchreis',
     '1. Milch in einem schweren Topf aufkochen.\n2. Rundkornreis und eine Prise Salz unterrühren.\n3. Hitze reduzieren und bei schwacher Hitze 30-35 Minuten köcheln lassen, gelegentlich umrühren.\n4. Wenn der Reis die Milch aufgesogen hat und cremig ist, Zucker und Vanillezucker unterrühren.\n5. Vom Herd nehmen und Butter unterrühren.\n6. Zugedeckt 10 Minuten ziehen lassen.\n7. Vor dem Servieren nochmals auflockern.\n8. Mit Zimt bestreut und Apfelmus servieren.',
-    '4 Portionen'
+    '4 Portionen',
+  'https://www.gutekueche.at/storage/media/recipe/102012/conv/einfacher-milchreis-default.jpg'
 );
  
      -- Rezept 1: Hörnli-Auflauf mit Zutaten 1-9
