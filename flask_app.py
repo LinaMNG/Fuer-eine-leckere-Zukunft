79273d1ed208394ef7877275129369df8912610d
EGM <elena.guthauser@mng.ch>
2026-01-17 13:26:22 +0000

    Menge vor Zutat

--- flask_app.py
+++ flask_app.py
@@ -226,24 +226,24 @@ def liked_recipes():
 		
 @app.route('/recipe/<int:recipe_id>')
 def recipe_detail(recipe_id):
-	recipe = db_read("SELECT * FROM recipes WHERE recipe_id=%s", (recipe_id,), single=True)
-	if recipe:
-		# Zutaten über die contains-Tabelle laden (nur ingredient_name)
-		ingredients = db_read(
-			"""
-			SELECT i.ingredient_name,
-			c.menge
-			FROM contains c
-			JOIN ingredient i ON c.ingredient_id = i.id
-			WHERE c.recipe_id = %s
-			ORDER BY i.ingredient_name
-			""",
-			(recipe_id,)
-		)
-		# db_read liefert Liste von dicts mit key 'ingredient_name'
-		recipe['ingredients'] = ingredients
-		return render_template('receipe_detail.html', recipe=recipe)
-	return render_template('receipe_detail.html', recipe=None, error="Rezept nicht gefunden"), 404
+    recipe = db_read("SELECT * FROM recipes WHERE recipe_id=%s", (recipe_id,), single=True)
+    if recipe:
+        ingredients = db_read(
+            """
+            SELECT i.ingredient_name,
+                   c.menge
+            FROM contains c
+            JOIN ingredient i ON c.ingredient_id = i.id
+            WHERE c.recipe_id = %s
+            ORDER BY i.ingredient_name
+            """, (recipe_id,)
+        )
+        
+        return render_template('recipe_detail.html', 
+                               recipe=recipe, 
+                               ingredients=ingredients)  # ← Wichtig!
+    else:
+        return "Rezept nicht gefunden", 404
 
 
 @app.route('/search')
