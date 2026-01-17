b96d547e5906e3dcc21352b0d2f679ced0d6ef19
EGM <elena.guthauser@mng.ch>
2026-01-17 13:06:26 +0000

    remvoe todos

--- flask_app.py
+++ flask_app.py
@@ -119,20 +119,9 @@ def logout():
 def index():
 	# GET
 	if request.method == "GET":
-		todos = db_read("SELECT id, content, due FROM todos WHERE user_id=%s ORDER BY due", (current_user.id,))
-		return render_template("main_page.html", todos=todos)
+		return render_template("main_page.html")
 
 	# POST
-	content = request.form["contents"]
-	due = request.form["due_at"]
-	db_write("INSERT INTO todos (user_id, content, due) VALUES (%s, %s, %s)", (current_user.id, content, due, ))
-	return redirect(url_for("index"))
-
-@app.post("/complete")
-@login_required
-def complete():
-	todo_id = request.form.get("id")
-	db_write("DELETE FROM todos WHERE user_id=%s AND id=%s", (current_user.id, todo_id,))
 	return redirect(url_for("index"))
 
 @app.route('/like_recipe/<int:recipe_id>', methods=['POST'])
